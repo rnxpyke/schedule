f741548c2559a8d63caad150d12783bb6f6fd0c0
rnxypke <rnxpyke@gmail.com>
2018-09-07 18:47:18 +0200

    create module

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::fs;
+use std::io;
+use std::env;
+use std::path::PathBuf;
+
+
+pub fn get_path() -> String {
+    let key = "HOME";
+    let home = match env::var(key) {
+        Ok(x) => x,
+        Err(e) => panic!(e)
+    };
+    home + "/.schedule"
+}
+
+pub fn init_store() {
+    fs::create_dir(get_path());
+}
+
+#[derive(Debug, Clone)]
+pub struct Project {
+    path: PathBuf
+}
+
+impl Project {
+    pub fn from(path: PathBuf) -> Project {
+        Project {
+            path
+        }
+    }
+}
+
+pub fn list_projects() -> Vec<Project> {
+    match list_projects_res() {
+        Ok(x) => x,
+        _ => vec!()
+    }
+}
+
+fn list_projects_res() -> io::Result<Vec<Project>> {
+    let dir = get_path();
+    
+    let mut projects: Vec<Project> = Vec::new();
+
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            projects.push(Project::from(path));
+        }
+    }
+    Ok(projects)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
+extern crate clap;
+extern crate schedule;
+
+
+use clap::{App,Arg,SubCommand};
+
+use schedule::*;
+
+use std::fs;
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("schedule")
+                    .subcommand(SubCommand::with_name("list")
+                        .about("list projects"))
+                    .get_matches();
+
+    init_store();
+
+    match matches.subcommand_name() {
+        Some("list") => {
+            println!("{:?}", list_projects());
+            },
+        _ => {}
+    }
 }
+
+
+
+
 
 
